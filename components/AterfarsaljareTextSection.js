7b35e047401b96e8dd4548c9928866a51445ebfc
greenplank <info@greenplank.eu>
2021-12-14 11:31:33 +0500

    newUpdate

--- components/AterfarsaljareTextSection.js
+++ components/AterfarsaljareTextSection.js
@@ -11,9 +11,9 @@ const AterfarsaljareTextSection = () => {
                 <div className="text-center block-title" >
                     <h5>Finden Sie einen Green Plank-Händler für Naturfaserverbundprodukte in Ihrer Nähe. Erfahren Sie, wo Sie Green Plank-Terrassendielen, Zäune, Verkleidungsmaterialien und die für die Fertigstellung Ihres neuen Projekts erforderlichen Materialien kaufen können.</h5>
                 </div>
-                <div>
+                {/* <div>
                 <iframe src="https://storage.googleapis.com/greenplankdata1/index.html" width="100%" height="600px" scrolling="no" frameborder="no" allowtransparency="true"></iframe>
-                </div>  
+                </div>   */}
             </Container>
         </section>
     );
